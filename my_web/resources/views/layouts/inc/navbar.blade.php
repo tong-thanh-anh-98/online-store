25fd628fcd98396ca2f226631d74e98548b86f31
Tống Thành Anh <>
2024-02-26 10:40:59 +0700

    fix admin panel template

--- my_web/resources/views/layouts/inc/navbar.blade.php
+++ my_web/resources/views/layouts/inc/navbar.blade.php
@@ -1,16 +1,9 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Right navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
               <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>					
     </ul>
-    <div class="navbar-nav pl-2">
-        <!-- <ol class="breadcrumb p-0 m-0 bg-white">
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol> -->
-    </div>
-    
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
@@ -18,11 +11,9 @@
             </a>
         </li>
         <li class="nav-item dropdown">
-
             <a class="nav-link p-0 pr-3" data-toggle="dropdown" href="#">
                 <img src="{{ asset('admin-assets/img/admin.png') }}" class='img-circle elevation-2' width="40" height="40" alt="">
             </a>
-
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3">
                 <h4 class="h4 mb-0"><strong>{{ Auth::guard('admin')->user()->name }}</strong></h4>
                 <div class="mb-3">{{ Auth::guard('admin')->user()->email }}</div>
